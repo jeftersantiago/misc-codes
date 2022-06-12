6f7eee792ac4e52e69fa7f47fff6dae2a6f78904
jefter66 <jefterrsantiago@gmail.com>
2022-06-11 23:52:45 -0300

    projeto_SO alterações

--- projeto_SO/shebang.sh
+++ projeto_SO/shebang.sh
@@ -3,15 +3,14 @@ file=$1
 # Checa se arquivo é executavel
 # Se não é, transforma o arquivo
 # em um executável.
-if [ -x "$file" ];
-then
-    echo "Arquivo '$file' : é executável"
-else
-    echo "Arquivo '$file' : não é executável"
-    # Torna aquivo executável 
-    echo "Tornando arquivo '$file' em executável"
-    chmod +x "$file"
-fi
+make_executable () {
+    if [ ! -x "$file" ]; then
+        echo "Arquivo '$file' : não é executável"
+        # Torna aquivo executável 
+        echo "Tornando arquivo '$file' em executável"
+        chmod +x "$file"
+    fi
+}
 # Armazena conteúdo da primeira linha do arquivo.
 read -r firstLine < $file
 # Checa se tem o shebang "#!" 
@@ -21,10 +20,13 @@ case $firstLine in
         shebang=true
 esac
 # Cria arquivo de backup se não tem shebang
-if ( ! $shebang );
-then
+if ( ! $shebang ); then
     # Cria o arquivo de backup com ".bak" no fim
     cp "$file" "$file".bak
     # Adiciona o shebang no inicio do arquivo.
-    sed -i '1 i\#!/bin/bash' "$file"
+    shebang="#!/bin/bash"
+    (echo $shebang && cat "$file") > /tmp/tmp_file && mv /tmp/tmp_file "$file" 
 fi
+# Apenas o arquivo original vai ser executável, o ".bak" vai continuar
+# como era antes (se era executável, continua executável).
+make_executable
